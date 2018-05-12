85eef3b85076f37dbf36a6c6fb107652cb39a576
Seokhwan Cheon <chaos314@gmail.com>
2018-05-12 21:09:40 +0900

    Updated description

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ dependencies = [
     'pyjwt'
 ]
 
-version = '0.2.0'
+version = '0.9.1'
 
-setup(name='python-upbit',
+setup(name='upbit',
       version=version,
       packages=['upbit'],
-      description='Python wrapper for the Upbit API',
+      description='Python wrapper for the Upbit REST API',
       url='https://github.com/chaos314/python-upbit',
       author='Seokhwan Cheon',
       author_email='chaos314@gmail.com',
@@ -21,12 +21,8 @@ setup(name='python-upbit',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.6',
-          'Development Status :: 3 - Alpha',
           'Topic :: Office/Business :: Financial',
       ],
 )
